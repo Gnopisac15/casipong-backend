718e01a286780a949d79f23b8c9b5f9eec51874f
2ndyrGroupB <2ndyrGroupB@pn.local>
2021-04-10 03:07:45 +0800

    Firts Commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const cors = require ('cors');
+
+const mongoose = require('mongoose');
+
+const UserRouter = require('./routes/user.js')
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5500;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+
+mongoose.connect(uri,{useNewUrlParser : true, useCreateIndex:true, useUnifiedTopology:true})
+
+const connection = mongoose.connection;
+
+connection.once('open', ()=>{
+    console.log('MongoDB connection establisha')
+})
+
+app.use('/user' , UserRouter);
+app.listen(port, () => {
+    console.log('server is running at port: '+ port);
+})
+
